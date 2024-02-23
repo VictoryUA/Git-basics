67c2e5c067879812bf193dbefcca2a0637d3c216
Victoria <vikpo6969@gmail.com>
2024-02-23 17:03:36 +0100

    Change the app to support division

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ def add(num1, num2):
 def subtract(num1, num2):
     return num1 - num2
 
-operation = input('Choose operation (add/subtract): ')
+def divide(num1, num2):
+    if num2 != 0:
+        return num1/num2
+    else:
+        return "Error: Division by zero"
+    
+operation = input('Choose operation (add/subtract/division): ')
 num1 = float(input('Enter first number: '))
 num2 = float(input('Enter second number: '))
 
@@ -14,5 +20,8 @@ if operation == 'add':
 elif operation == 'subtract':
     result = subtract(num1, num2)
     print('Result: ', result)
+elif operation == 'division':
+    result = division(num1, num2)
+    print('Result: ', result)
 else:
-    print("Invalid operation. Please choose 'add' or 'subtract'.")
+    print("Invalid operation. Please choose 'add', 'subtract' or 'division'.")
